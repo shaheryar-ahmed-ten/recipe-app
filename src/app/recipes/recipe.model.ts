b2ff820d7ca2dc8e6551d8dd762ddda3366e67f7
Shaheryar Ahmed <raza.anis@qavitechnologies.com>
2022-11-06 20:41:29 +0500

    commit2

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -10,7 +10,7 @@ export class Recipe {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
-        this.ingredients = ingredients
+        this.ingredients = ingredients;
     }
 
 }
